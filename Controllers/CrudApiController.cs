37fd7e12981f247ad7579ab0e9f5176c6ff0b455
ksr1319 <62940770+ksr1319@users.noreply.github.com>
2021-04-21 14:37:19 +0530

    Add files via upload

---
+++ Controllers/CrudApiController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using CrudOperationsUsingWebAPI.Models;
+
+namespace CrudOperationsUsingWebAPI.Controllers
+{
+    [RoutePrefix("api/CrudApi")]
+    public class CrudApiController : ApiController
+    {
+
+        satheeshdbEntities satheeshdbEntities = new satheeshdbEntities();
+        [HttpGet]
+        public IEnumerable<customer> GetCustomers()
+        {
+            return satheeshdbEntities.customers.ToList();
+        }
+        [HttpGet]
+        public customer GetCustomerById(int id)
+        {
+            return satheeshdbEntities.customers.FirstOrDefault(x=>x.customerno==id);
+        }
+        [HttpPost]
+        [Route("Post")]
+        public IHttpActionResult Post([FromBody] customer customer)
+        {
+            using (satheeshdbEntities satheeshdbEntities = new satheeshdbEntities())
+            {
+                satheeshdbEntities.customers.Add(customer);
+                satheeshdbEntities.SaveChanges();
+                return Ok(customer);
+            }
+
+        }
+        [HttpDelete]
+       
+        public IHttpActionResult Delete(int id)
+        {
+            using (satheeshdbEntities satheeshdbEntities = new satheeshdbEntities())
+            {
+                satheeshdbEntities.customers.Remove( satheeshdbEntities.customers.FirstOrDefault(x => x.customerno == id));
+                satheeshdbEntities.SaveChanges();
+                return Ok();
+            }
+
+        }
+        public IHttpActionResult Put(int id, [FromBody] customer customer)
+        {
+
+            using (satheeshdbEntities satheeshdbEntities = new satheeshdbEntities())
+            {
+               var entity= satheeshdbEntities.customers.FirstOrDefault(x => x.customerno == id);
+
+                entity.customeradress = customer.customeradress;
+                entity.name = customer.name;
+                satheeshdbEntities.SaveChanges();
+                return Ok(customer);
+            }
+        }
+
+
+
+    }
+}
+
